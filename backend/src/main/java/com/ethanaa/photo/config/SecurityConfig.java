eff3f60a2edee397bb55571720bce8e243c180c8
Ethan Anderson <ethanaa@gmail.com>
2018-08-13 07:13:04 -0500

    add ssl

--- backend/src/main/java/com/ethanaa/photo/config/SecurityConfig.java
+++ backend/src/main/java/com/ethanaa/photo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ethanaa.photo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
 
 @Configuration
 @EnableWebSecurity
@@ -36,8 +38,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("USER", "ADMIN");
     }
 
+    @Value("${server.ssl.key-store:}")
+    private String sslKeyStore;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        if (!StringUtils.isEmpty(sslKeyStore)) {
+            http
+                    .requiresChannel()
+                    .anyRequest().requiresSecure();
+        }
 
         http
                 .csrf().disable()
